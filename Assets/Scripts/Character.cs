e6c5832b366c7502e9d109e6fcc7fd6017c2425d
Ashen <michael.hrabanek@solvertech.cz>
2019-07-21 19:55:33 +0200

    Time control

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,33 +5,63 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     // Start is called before the first frame update
-    float movementSpeed = 3f;
     List<Vector2Int> target;
+    private TimeControl tc;
+    private float speed = 1f;
+    private float accumulatedTime;
+    private Vector3 startingPoint;
     void Start()
     {
+        startingPoint = transform.position;
         target = new List<Vector2Int>();
+        tc = GameObject.FindGameObjectWithTag("TimeController").GetComponent<TimeControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        accumulatedTime += Time.deltaTime * tc.TimePointMultiplier();
+        //in case of more then one time point from last frame, skip rendering of previous actions
+        while (accumulatedTime > 1f)
+        {
+            Execute();
+        }
+
         if (target.Count > 0)
         {
-            var next = target[0];
-            var nextVector3 = Geometry.PointFromGrid(next);
+            var nextVector3 = Geometry.PointFromGrid(target[0]);
             Step(nextVector3);
-            if (Mathf.Approximately(transform.position.x, nextVector3.x) && Mathf.Approximately(transform.position.z, nextVector3.z))
-            {
-                target.RemoveAt(0);
-            }
         }
     }
-    public void Move(List<Vector2Int> path) 
+
+    public void Move(List<Vector2Int> path)
     {
+        accumulatedTime = 0;
         target = path;
     }
     void Step(Vector3 to)
     {
-        transform.position = Vector3.MoveTowards(transform.position, to, movementSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(startingPoint, to, accumulatedTime);
+    }
+
+    private void Execute()
+    {
+        if (target.Count > 0)
+        {
+            startingPoint = Geometry.PointFromGrid(target[0]);
+            transform.position = startingPoint;
+            target.RemoveAt(0);
+            if (target.Count == 0)
+            {
+                //TODO add action for destination point
+            }
+        }
+
+        accumulatedTime -= 1f;
+        if (accumulatedTime < 0)
+        {
+            //float numbers overflow check
+            accumulatedTime = 0f;
+        }
     }
 }
