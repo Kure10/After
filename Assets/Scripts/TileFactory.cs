e0d71b50622733804b2866801b409cff5fef24f9
Ashen <michael.hrabanek@solvertech.cz>
2019-07-29 20:55:35 +0200

    Disable to place new building on debris

--- Assets/Scripts/TileFactory.cs
+++ Assets/Scripts/TileFactory.cs
@@ -178,7 +178,7 @@ public class TileFactory : MonoBehaviour
     public bool Buildable(Vector2Int coord)
     {
         bool ret = false;
-        if (grid[coord.x, coord.y] is Tile tile)
+        if (grid[coord.x, coord.y] is Tile tile && !(grid[coord.x, coord.y] is DebrisTile))
         {
             if (tile.building == null)
             {
