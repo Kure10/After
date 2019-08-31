7a1c141016f7296874ecc325759fbf2e953c4386
matous <mudrik.m@seznam.cz>
2019-08-31 20:50:22 +0200

    1. keyCode Exit na panely.
    2. Cheat panel.   Přidan

--- Assets/Scripts/Panels/PanelControler.cs
+++ Assets/Scripts/Panels/PanelControler.cs
@@ -6,26 +6,60 @@ using UnityEngine.UI;
 public class PanelControler : MonoBehaviour
 {
 
-    public GameObject panel;
-    private bool isActive = false;
+    [SerializeField] GameObject cheatPanel;
+    public GameObject panel; // ToDo Pridam vsechny panely a pak vymyslim lepsi mechanismus zavirani a vypinani.
+    public bool isActive = false;
+    [SerializeField] List<GameObject> panels = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        panel.SetActive(isActive);
+        AddToList();
+        panel.SetActive(isActive); // ToDo to souvisi stim prvnim
+        cheatPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        KeyControl();
     }
 
 
     public void TurnOn()
     {
         isActive = !isActive;
-        panel.SetActive(isActive);
+        panel.SetActive(isActive);  // ToDo to souvisi stim prvnim
+    }
+
+
+    public void KeyControl ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            foreach (var item in panels)
+            {
+                if (item.activeSelf)
+                {
+                    item.SetActive(false);
+                }
+            }
+            if(isActive)
+            isActive = !isActive;
+        }
+    }
+
+    public void AddToList()
+    {
+        foreach (GameObject items in GameObject.FindGameObjectsWithTag("ExtensionPanel"))
+        {
+            panels.Add(items);
+        }
+    }
+
+    public void ShowCheats()
+    {
+        cheatPanel.SetActive(!cheatPanel.activeSelf);
     }
 
 }
