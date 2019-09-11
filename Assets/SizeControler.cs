550bd9cbfce17cb6c87526c45daf4a72395e6a30
matous <mudrik.m@seznam.cz>
2019-09-11 21:54:20 +0200

    1. Gui Vystavba upgrade.
    (Nefe podivejse prosim.
    Abych vedel jestli jdu spravným smerem.)

---
+++ Assets/SizeControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeControler : MonoBehaviour
+{
+    private Image[] images;
+
+    [Header("Size of Building")]
+    public bool[] myArray = new bool[] { true, true, false, true, false, true };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        images = GetComponentsInChildren<Image>();
+        UpdateSize();
+    }
+
+    private void UpdateSize()
+    {
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] == true)
+                images[i].color = Color.black;
+        }
+    }
+
+}
