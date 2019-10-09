89c570fc252db182e1d02005a32860ddff04afb4
matous <mudrik.m@seznam.cz>
2019-10-09 21:12:35 +0200

    1. Specialiste data .  -> finito

---
+++ Assets/Scripts/Specialists/Editor/SpecialistEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Specialists))]
+public class SpecialistEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Specialists spec = (Specialists)target;
+
+
+
+        spec.maxStamina = 57600 + 2400 * spec.level;
+        spec.maxZdravy = 40 + spec.level + 4 * spec.vojak + 2 * spec.technik + spec.vedec + spec.social;
+
+
+    }
+
+}
--- Assets/Scripts/Specialists/Specialists.cs
+++ Assets/Scripts/Specialists/Specialists.cs
@@ -7,20 +7,30 @@ using UnityEngine.UI;
 [CreateAssetMenu (menuName = "Specialista", fileName = "No name!") ]
 public class Specialists : ScriptableObject
 {
-    private enum Povolani { Doktor, Vojak, Vojín, Vedec, ZdravotniSestra, Seržant, };
+    private enum Povolani { Doktor, Vojak, Vojín, Vedec, ZdravotniSestra, Seržant, JadernýFyzik, SpecNaz, Kuchař, Stavitel, Programator };
 
-
+    [Header("Identification")]
     [SerializeField] private Image image = null;
     [SerializeField] private string fullName = "Pavel";
     [SerializeField] Povolani povolani;
-    // [SerializeField] private string povolani = "No Spec Tady by mel byt asi enum jeste uvidim";
-
-    [Range(0, 20)] [SerializeField] private int level = 0;
-    [Range(0,10)] [SerializeField]  private int vojak = 0;
-    [Range(0, 10)] [SerializeField] private int vedec = 0;
-    [Range(0, 10)] [SerializeField] private int technik = 0;
-    [Range(0, 10)] [SerializeField] private int social = 0;
+    [Header("Main attributes")]
+    [Range(0, 20)] [SerializeField] public int level = 0;
+    [Range(0,10)] [SerializeField] public int vojak = 0;
+    [Range(0, 10)] [SerializeField] public int vedec = 0;
+    [Range(0, 10)] [SerializeField] public int technik = 0;
+    [Range(0, 10)] [SerializeField] public int social = 0;
     [SerializeField] private int karma = 0;
+
+    [Header("Health and Stamina")]
+    [SerializeField] public int maxStamina;
+    [SerializeField] public int maxZdravy;
+    [Space]
+    [SerializeField] public int currentStamina = 1;
+    [SerializeField] public int currentZdravy = 1;
+
+
+    // obnova zdravy a staminy neni dodelana v GameDe Doc. zatim
+
 
     // tohle se jeste musi rozmyslet jestli to vubec bude tady...
     private int[] pointPerHour = { 5, 10, 20, 40, 80, 160, 320, 640, 1280, 2560 };
@@ -28,5 +38,19 @@ public class Specialists : ScriptableObject
     private void Awake()
     {
         povolani = Povolani.Doktor;
+        CalcHealth(level,vojak,technik,vedec,social);
+        CalcStamina(level);
     }
+
+    private void CalcStamina(int lvl)
+    {
+        maxStamina = 57600 + 2400 * lvl;
+    }
+
+    private void CalcHealth(int lvl, int mil, int tel, int scl, int sol)
+    {
+        maxZdravy = 40 + lvl + 4 * mil + 2 * tel + scl + sol;
+    }
+
+    
 }
