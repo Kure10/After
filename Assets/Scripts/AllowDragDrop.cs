c877c06e6111d1f5becab49fd19cad6743b9889c
matous <mudrik.m@seznam.cz>
2019-10-16 21:41:56 +0200

    1. Dodelavam nejaky kraviny. zkousel jsem drag and drop

---
+++ Assets/Scripts/AllowDragDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AllowDragDrop : MonoBehaviour , IDragHandler
+{
+
+    [SerializeField]
+    private bool dragParent = false;
+
+    public Transform parent;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(!dragParent)
+        transform.position = eventData.position;
+
+        if(dragParent)
+        {
+            parent.transform.position = eventData.position;
+        }
+    }
+
+    private void Awake()
+    {
+        if (transform.parent.parent.parent != null)
+        {
+            parent = this.transform.parent.parent.parent;
+            if(parent.name == "Canvas")
+            {
+                parent = this.transform.parent.parent;
+            }
+        }
+
+        else if (transform.parent.parent != null)
+            parent = this.transform.parent.parent;
+
+        else if (transform.parent != null)
+            parent = this.transform.parent;
+
+
+    }
+
+}
