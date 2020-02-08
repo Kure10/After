a8bda969ed5626f96510bedadf224f3d21bffffd
Kure <Mudrik.ma@seznam.cz>
2020-02-08 17:39:14 +0100

    1. Pokrok

---
+++ Assets/Scripts/TESTING_EVENTS/uWindowMission.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class uWindowMission : MonoBehaviour
+{
+
+
+    private string _missionName;
+
+    private Sprite sprite;
+
+    private string missionType;
+
+    private float missionDistance;
+
+    private LevelOfDangerous missionLevel;
+
+    private MissionEnviroment missionEnviroment;
+
+    private MissionTime missionTime;
+
+    #region Properties
+
+    public string MissionName
+    {
+        get { return _missionName; }
+        set { _missionName = value; }
+    }
+
+    #endregion
+
+}
