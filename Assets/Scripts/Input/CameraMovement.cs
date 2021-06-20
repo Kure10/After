a786b90087e55d789a0b3b27173f3ee801d8d4e8
Kure <Mudrik.ma@seznam.cz>
2021-06-20 19:10:36 +0200

    Camera mouseControll

--- Assets/Scripts/Input/CameraMovement.cs
+++ Assets/Scripts/Input/CameraMovement.cs
@@ -9,6 +9,7 @@ public class CameraMovement : MonoBehaviour
     [Space]
 
     public float CameraSpeed = 9f;
+    public float CameraMouseSpeed = 3f;
     public float MinY = 10f;
     public float MaxY = 40f;
 
@@ -18,6 +19,12 @@ public class CameraMovement : MonoBehaviour
     private static bool scrollEnabled = true;
     private static bool movementEnabled = true;
     // Start is called before the first frame update
+
+    Vector3 mouseDelta = new Vector3(0, 0, 0);
+    Vector3 MouseStart = new Vector3(0, 0, 0);
+    private bool bDragging = false;
+    float dist = 5f;
+
     void Start()
     {
         movement = new Vector3();
@@ -26,72 +33,98 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xAxis = 0;
-        zAxis = 0;
-        var speed = CameraSpeed;
-        if (Input.GetKey(KeyCode.LeftShift))
+        bool mouseState = Input.GetKey(KeyCode.Mouse0);
+
+        if (Input.GetMouseButtonDown(0))
         {
-            speed = CameraSpeed * 6f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            xAxis = -speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            xAxis = speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            zAxis = speed * Time.deltaTime;
-        } 
-        else if (Input.GetKey(KeyCode.S)) {
-            zAxis = -speed * Time.deltaTime;
-        }
-        movement.x = xAxis;
-        movement.z = zAxis;
-        if (scrollEnabled)
-        {
-            movement.y = Input.mouseScrollDelta.y * Time.deltaTime * speed * -3;
-        } else
-        {
-            movement.y = 0;
+            MouseStart = Input.mousePosition;
+
         }
 
-        if (!movementEnabled)
+        if (Input.GetMouseButton(0))
         {
-            movement.x = 0;
-            movement.z = 0;
+            Vector3 mouseDelta = Input.mousePosition - MouseStart;
+            float dx = (mouseDelta.x) * (0.0001f * CameraMouseSpeed);  
+            float dy = (mouseDelta.y) * (0.0001f * CameraMouseSpeed); 
+
+            if (dx != 0 | dy != 0)
+            {
+                movement = new Vector3(-dx, 0, -dy);
+                cameraHolder.transform.Translate(movement);
+            }
         }
 
-        Vector3 vec = cameraHolder.transform.position;
-
-        if (vec.y > MaxY || vec.y < MinY)
+        if(!mouseState)
         {
-            movement.y = 0;
+            xAxis = 0;
+            zAxis = 0;
+            var speed = CameraSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = CameraSpeed * 6f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                xAxis = -speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                xAxis = speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                zAxis = speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                zAxis = -speed * Time.deltaTime;
+            }
+            movement.x = xAxis;
+            movement.z = zAxis;
+
+            if (scrollEnabled)
+            {
+                movement.y = Input.mouseScrollDelta.y * Time.deltaTime * speed * -3;
+            }
+            else
+            {
+                movement.y = 0;
+            }
+
+            if (!movementEnabled)
+            {
+                movement.x = 0;
+                movement.z = 0;
+            }
+
+            Vector3 vec = cameraHolder.transform.position;
+
+            if (vec.y > MaxY || vec.y < MinY)
+            {
+                movement.y = 0;
+            }
+
+            bool isMaxZoom = false;
+            bool isMinZoom = false;
+
+            if (vec.y <= MinY)
+            {
+                vec.y = MinY;
+                cameraHolder.transform.position = new Vector3(vec.x, 10 /*MinY + 0.01f*/, vec.z);
+                isMinZoom = true;
+            }
+            else if (vec.y >= MaxY)
+            {
+                vec.y = MaxY;
+                cameraHolder.transform.position = new Vector3(vec.x, 40 /*MaxY + 0.01f*/, vec.z);
+                isMaxZoom = true;
+            }
+
+            cameraPositionY = cameraHolder.transform.position.y; // properity for healthBar
+
+            if ((movement.y <= 0 && !isMinZoom) || (movement.y >= 0 && !isMaxZoom))
+                cameraHolder.transform.Translate(movement);
         }
-
-        bool isMaxZoom = false;
-        bool isMinZoom = false;
-
-        if (vec.y <= MinY)
-        {
-            vec.y = MinY;
-            cameraHolder.transform.position = new Vector3(vec.x, 10 /*MinY + 0.01f*/, vec.z);
-            isMinZoom = true;
-        }
-        else if (vec.y >= MaxY)
-        {
-            vec.y = MaxY;
-            cameraHolder.transform.position = new Vector3(vec.x, 40 /*MaxY + 0.01f*/, vec.z);
-            isMaxZoom = true;
-        }
-
-        cameraPositionY = cameraHolder.transform.position.y; // properity for healthBar
-
-        if ((movement.y <= 0 && !isMinZoom) || (movement.y >= 0 && !isMaxZoom))
-            cameraHolder.transform.Translate(movement);
-
     }
     public static void ZoomByScrollEnabled(bool enabled)
     {
